66dcd8c489fca10b5c21140be048abdb242b371a
meteomate.sdu.dk <meteomate@noreply.com>
2025-09-09 11:01:41 +0200

    change to prod url

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,7 +1,8 @@
 // TODO: Set from .env
 // const API_URL = process.env.REACT_APP_API_URL
 // const API_URL = "http://0.0.0.0:80" 
-const API_URL = "http://localhost:80" 
+// const API_URL = "http://localhost:80"
+const API_URL = "https://meteomate.sdu.dk"
 
 export {
     API_URL
